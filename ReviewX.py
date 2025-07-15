77d1d9a160d32941cbf5ef01ab594ccebd4c6e40
JACOBIAN01 <subhadeepghorai23@gmail.com>
2025-07-16 02:08:54 +0530

    Update

--- ReviewX.py
+++ ReviewX.py
@@ -5,6 +5,8 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 import time
 import random
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 class CodingalReviewer:
     def __init__(self, number, password, log_callback=None,review_callback = None):
@@ -41,47 +43,37 @@ class CodingalReviewer:
     
     def login(self):
         try:
-            self.update("Opening Codingal Website...")
+            self.update("🚀 Launching the Codingal universe... Buckle up!")
             self.driver.get("https://www.codingal.com/")
             time.sleep(1)
-            self.update("Finding Login Button..")
             login_button = self.wait.until(EC.element_to_be_clickable((By.XPATH, "//button[text()='Login']")))
             login_button.click()
             time.sleep(1)
-            self.update("Login Button Found. Trying to enter Phone Number")
+            self.update("Logging in to your dashboard")
             phone_input = self.wait.until(EC.presence_of_element_located((By.NAME, "phone")))
             phone_input.send_keys(self.number)
-            time.sleep(1)
-            self.update("Finding Login with Password Button")
+            time.sleep(0.5)
             login_btn_1 = self.wait.until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(), 'Login with password')]")))
             login_btn_1.click()
             time.sleep(0.5)
-            self.update("Entering Password")
             password_input = self.wait.until(EC.element_to_be_clickable((By.NAME, "password")))
             password_input.send_keys(self.password)
-            time.sleep(0.5)
-            self.update("Login Button Cicked!")
+            time.sleep(0.25)
             login_btn_2 = self.wait.until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(), 'Login with password')]")))
             login_btn_2.click()
-            time.sleep(1)
-
-            self.update("Opening Your Dashboard...")
-            time.sleep(3)
+            time.sleep(0.25)
             profile_pic = self.wait.until(EC.presence_of_element_located((By.XPATH, "//img[@alt='Profile Image']")))
             profile_pic_src = profile_pic.get_attribute("src")
-            time.sleep(0.5)
-            self.update("Profile Pic Collected...")
+            time.sleep(0.25)
             teacher_name_elem = self.wait.until(EC.presence_of_element_located((By.XPATH, "//div[contains(@class, 'flex-col')]/h2")))
             teacher_name = teacher_name_elem.text
             time.sleep(0.25)
-            self.update("Name Identified...")
             self.store_user_details(profile_pic_src, teacher_name)
             time.sleep(0.25)
-            
-            self.update(f"Login Successful! Hello {teacher_name}")
+            self.update(f"Login successful. Welcome aboard, Captain {teacher_name}!")
         except Exception as e:
-            self.update(f"Error During Login: Please Check Your Credentials Again.{e}")
-            self.update("Please Cancel & Try Again!")
+            self.update(f"Error During Login: Please Check Your Credentials Again.")
+       
     
     def pending_project_count(self):
         self.driver.get("https://www.codingal.com/teacher/dashboard/projects/")
@@ -92,7 +84,7 @@ class CodingalReviewer:
             )
             return int(project_number_elem.text)
         except Exception:
-            self.update("Could not find project count, assuming 0")
+            self.update("Hmm... Can't find any projects. Maybe it's time for a coffee?")
             return 0
     
     def generate_review(self, name, lesson):
@@ -137,7 +129,7 @@ class CodingalReviewer:
 
     def review_project(self):
         try:
-            self.send_review_tracker_update("Review Started")
+            self.send_review_tracker_update("Review engine warming up...")
             time.sleep(0.25)
             element = self.wait.until(EC.element_to_be_clickable((By.XPATH, "(//a[contains(text(), 'Review now')])[1]")))
             self.driver.execute_script("arguments[0].scrollIntoView(true);", element)
@@ -147,11 +139,12 @@ class CodingalReviewer:
             student_name_elem = self.wait.until(EC.presence_of_element_located((By.XPATH, "//p[contains(text(), 'Submitted by')]/preceding-sibling::p")))
             student_name = student_name_elem.text
 
-            self.send_review_tracker_update(f"Analyzing Project for {student_name}")
-            
-            lesson_name_elem = self.wait.until(EC.presence_of_element_located((By.XPATH, "//p[contains(text(), 'Lesson')]")))
-            lesson_name = lesson_name_elem.text
-            
+            self.send_review_tracker_update(f"Scanning {student_name}'s masterpiece...")
+
+            lesson_name_elem = self.driver.find_element(By.XPATH, "//div[@class='flex items-center flex-wrap']/p[contains(@class, 'text-xl') and contains(@class, 'font-600')]")
+            lesson_name =  lesson_name_elem .text
+
+
             review_btn = self.wait.until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(), 'Review now')]")))
             review_btn.click()
             time.sleep(0.5)
@@ -159,14 +152,14 @@ class CodingalReviewer:
             textarea = self.wait.until(EC.presence_of_element_located((By.TAG_NAME, "textarea")))
             review = self.generate_review(student_name, lesson_name)
             textarea.send_keys(review)
-            self.send_review_tracker_update(f"Generating Review for {lesson_name}")
+            self.send_review_tracker_update(f"Crafting thoughtful feedback for Lesson: {lesson_name}")
             try:
                 enhance_btn = self.wait.until(EC.element_to_be_clickable((By.XPATH,"//button[.//span[text()='Enhance with AI']]")))
                 enhance_btn.click()
                 time.sleep(3)
             except Exception:
                 pass
-            self.send_review_tracker_update("Analyzing Rating")
+            self.send_review_tracker_update("Calculating star power")
             stars = self.wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "rating-star")))
             given_stars = random.choice([3,4])  # zero-based index; clicks 4th or 5th star
             stars[given_stars].click()
@@ -175,7 +168,7 @@ class CodingalReviewer:
             submit_btn.click()
             self.send_review_tracker_update("Review Submitted")
             time.sleep(0.5)
-            self.update(f"Review completed successfully for {student_name}.")
+            self.update(f"Feedback sent successfully to {student_name}.")
             back_to_projects = self.wait.until(EC.element_to_be_clickable((By.XPATH, "(//a[contains(text(), 'Back to projects')])")))
             back_to_projects.click()
             time.sleep(1)
@@ -184,6 +177,7 @@ class CodingalReviewer:
 
     def start_review(self):
         chrome_options = Options()
+
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--no-sandbox")
@@ -194,22 +188,23 @@ class CodingalReviewer:
         chrome_options.add_argument("--remote-debugging-port=9222")
         chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64)...")
         
-        self.driver = webdriver.Chrome(options=chrome_options)
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=chrome_options)
         self.wait = WebDriverWait(self.driver, 10)
         
         self.login()
         self.review_cancel = False
-        
+      
+      
         while True:
             pending_projects = self.pending_project_count()
             self.project_count = pending_projects
             
             if pending_projects == 0:
-                self.update("All projects review completed.")
+                self.update("Mission accomplished! All projects are reviewed. Time to celebrate")
                 break
             
             if self.review_cancel:
-                self.update("Review cancelled.")
+                self.update("Review paused. All engines cooling down...")
                 break
             
             try:
